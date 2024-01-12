979575760dc8c3ef148c7cdd61e689636ab2fe0a
Namra-23 <namrajoshi2303@gmail.com>
2024-01-12 18:40:56 +0530

    basic Form

--- Form/src/app/app.component.ts
+++ Form/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Form';
+  title = 'Namra joshi';
+  userLogin(item:any){
+    console.warn(item);
+    
+  }
 }
