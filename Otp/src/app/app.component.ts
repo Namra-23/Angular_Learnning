972fe3f0687c1e7a595ab700f72a96db1c5d8a4b
Namra-23 <namrajoshi2303@gmail.com>
2024-02-12 11:23:58 +0530

    Otp_verification - NJ

---
+++ Otp/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet,FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Otp';
+}
---
+++ Otp/src/app/otp-verification/otp-verification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-otp-verification',
+  standalone: true,
+  imports: [],
+  templateUrl: './otp-verification.component.html',
+  styleUrl: './otp-verification.component.css'
+})
+export class OtpVerificationComponent {
+
+}
