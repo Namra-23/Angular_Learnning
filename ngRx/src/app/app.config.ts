b05b63bb7eb97f4fd180174c3c3a244ecb614f9f
Namra-23 <namrajoshi2303@gmail.com>
2024-02-21 18:23:37 +0530

    redux-devtools - NJ

--- ngRx/src/app/app.config.ts
+++ ngRx/src/app/app.config.ts
@@ -1,11 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './counter/state/counter.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(),provideStore({counter : counterReducer})]
+  providers: [provideRouter(routes), provideClientHydration(),provideStore({counter : counterReducer}),provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
 };
