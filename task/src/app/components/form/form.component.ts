7213a197766e9e6d0c6cd840b094186e329cf58a
Namra-23 <namrajoshi2303@gmail.com>
2024-03-04 15:51:02 +0530

    form connected with ag-grid - NJ

--- task/src/app/components/form/form.component.ts
+++ task/src/app/components/form/form.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AgGridAngular, AgGridModule } from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
+
+interface RowData {
+  docTitle: string;
+  docVersion: string;
+  docStatus: string;
+  docType: string;
+}
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, AgGridAngular,AgGridModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
   myForm: FormGroup;
+  rowData : RowData[] = [];
+  colDefs: ColDef[] = [
+    { headerName: 'DocTitle', field: 'docTitle' },
+    { headerName: 'DocVersion', field: 'docVersion' },
+    { headerName: 'DocStatus', field: 'docStatus' },
+    { headerName: 'DocType', field: 'docType' }
+  ]
+
   demoObj = {
     'docTitle': 'software management',
     'docVersion': '1',
@@ -30,7 +47,10 @@ export class FormComponent {
   //On Submit Form
   onSubmit() {
     if (this.myForm.valid) {
-      console.log(this.myForm.value);
+      const formData = this.myForm.value;
+      this.rowData.push(formData);
+      this.rowData = [...this.rowData];
+      console.log(formData);
       this.myForm.reset();
     }
   }
@@ -38,5 +58,9 @@ export class FormComponent {
   //On Reset Form
   onReset() {
     this.myForm.reset();
+  }
+  defaultColDef = {
+    flex: 1,
+    minWidth: 100
   }
 }
