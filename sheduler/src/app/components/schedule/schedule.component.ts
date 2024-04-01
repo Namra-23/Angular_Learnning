e676723554f35b34a1e82e7e1ce0041d5441aaa0
Namra-23 <namrajoshi2303@gmail.com>
2024-04-01 13:50:55 +0530

    minor changes in schedular

--- sheduler/src/app/components/schedule/schedule.component.ts
+++ sheduler/src/app/components/schedule/schedule.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-schedule',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './schedule.component.html',
   styleUrl: './schedule.component.css'
 })
 export class ScheduleComponent {
+  weeklyHours: number = 0;
+  totalSubjects: number = 0;
+  professorList: string = '';
+  timetableGenerated: boolean = false;
+  timetable: any[] = [];
 
+  constructor() {}
+
+  generateTimetable() {
+    this.timetable = [];
+
+    const professors = this.professorList.split('\n');
+
+    const hoursPerSubject = Math.floor(this.weeklyHours / this.totalSubjects);
+
+    for (let i = 0; i < this.totalSubjects; i++) {
+      const professorIndex = i % professors.length;
+      const subject = `Subject ${i + 1}`;
+      const professor = professors[professorIndex];
+      const hours = hoursPerSubject;
+      this.timetable.push({ subject, professor, hours });
+    }
+
+    this.timetableGenerated = true;
+  }
 }
